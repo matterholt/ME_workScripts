20d811267d788838e9bc12c9db771db5138d67a4
MAtterholt <matterholt84@gmail.com>
2018-11-02 00:05:12 -0400

    more file organization

--- dataExtracting.py
+++ dataExtracting.py
@@ -5,13 +5,20 @@ Created on Wed Jan 17 20:45:10 2018
 
 @author: matterholt
 
+
+REV ONE, 
+needs to be refractored
+
+"""
+
+
 #the Data
 u = (0,1),(1,4),(2,9),(3,17),(4,27),(5,50)
 rf= (0,5),(1,12),(2,17),(3,30),(4,45),(5,70)
 ####
 
 
-
+# help create the curve, 
 def curve (u,rf):
     curveList = []
     for i, j in u:
