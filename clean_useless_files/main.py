53e533423a06b134167e6658e7ac24c402b8736a
MAtterholt <matterholt84@gmail.com>
2020-05-29 18:43:34 -0400

    update lint

--- clean_useless_files/main.py
+++ clean_useless_files/main.py
@@ -9,31 +9,32 @@ import datetime
 
 from time import strftime, gmtime, sleep
 
+
 def split_join_string(dir_path):
     path_sting = dir_path.split(':\\')
     s = '-'
     return s.join(path_sting)
-    
+
 
 def save_data_csv(dict_stat, current_date):
     dir_check = split_join_string(dict_stat['file_path'])
-    
+
     current_work_dir = os.getcwd()
-    saved_dir_file_name = f"{current_work_dir}\\checks_{dir_check}_{current_date}.csv"
-    
+    saved_dir_file_name = f"{current_work_dir}\\checks_{dir_check} _{current_date}.csv"
+
     try:
-        with open(saved_dir_file_name,'w') as csv_file:
+        with open(saved_dir_file_name, 'w') as csv_file:
 
             for [key, val] in dict_stat.items():
-                csv_file.write("%s,%s\n"%(key,val))
-            
-            
+                csv_file.write("%s,%s\n" % (key, val))
+
             print(f"\nCSV_Saved: \n{saved_dir_file_name}\n")
-            
+
     except IOError:
         print("I/O error")
-        
+
     return
+
 
 def print_time_took(time):
     """ print the correct time unit for the execution code """
@@ -72,9 +73,9 @@ def remove_files(file):
         os.remove(file)
     except PermissionError:
         print("Error while deleting file ", file)
-            
+
     return
-    
+
 
 def search_dir(dir_path, inputs_bond):
 
@@ -84,8 +85,6 @@ def search_dir(dir_path, inputs_bond):
 
     accumulated_file_size = 0
     files_path_error = 0
-    
-
 
     for subdir, dirs, files in os.walk(dir_path):
 
@@ -93,14 +92,13 @@ def search_dir(dir_path, inputs_bond):
 
             try:
                 file_path = subdir + os.sep + file_name
-                
-                
-                # TODO-> check files that are 2 or so years old, 
-                # TODO-> check duplicate files 
+
+                # TODO-> check files that are 2 or so years old,
+                # TODO-> check duplicate files
                 for extensions in inputs_bond["extension_no_need"]:
                     """ 
-                    Reviews the file extension, if found in list will document 
-                    specs and proced to remove from the directory
+                    Reviews the file extension, if found in list will
+                     documentspecs and proceed to remove from the directory
                     """
                     if file_path.endswith(extensions):
 
@@ -116,48 +114,44 @@ def search_dir(dir_path, inputs_bond):
                         # count the file size
                         file_size = file_size_KB(file_path)
                         accumulated_file_size += file_size
-                        
+
                         # REMOVE FILES ONCE DATA IS STORED
-                        if inputs_bond['want_to_remove'] :
+                        if inputs_bond['want_to_remove']:
                             print('will remove!!!')
                             remove_files(file_path)
-
 
             except FileNotFoundError:
                 files_path_error += 1
 
     search_info.update(file_path=dir_path)
-    search_info.update(GB_size= round(accumulated_file_size/1000000000,2))  # convert to GB
+    search_info.update(GB_size=round(
+        accumulated_file_size/1000000000, 2))  # convert to GB
     search_info.update(failed_path=files_path_error)
-    
+
     search_info.update(extension_count)
     search_info.update(access_date)
-    
+
     return search_info
 
 
 def main():
-    
+
     now_date_time = datetime.datetime.now()
     date_check = now_date_time.strftime("%Y-%m-%d")
-    
+
     boundaries = {
-        "want_to_remove" : False,
+        "want_to_remove": False,
         "extension_no_need": [".html", ".out", ".res", ".stat",
                               ".mesg", ".mvw"],
         "extension_remove_old": [".op2", ".odb", ".hyp", ".h3d"]
     }
-    
 
     raw_dir_path = r"F:"
 #    raw_dir_path = r"S:/v2"
 
     found_files_info = search_dir(raw_dir_path, boundaries)
     terminal_print(found_files_info)
-    save_data_csv(found_files_info,date_check)
-
-
-
+    save_data_csv(found_files_info, date_check)
 
     return found_files_info
 
