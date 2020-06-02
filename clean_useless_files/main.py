ae7d78dfd85f0615759752ae1d8c624ec8ac54c0
MAtterholt <matterholt84@gmail.com>
2020-06-01 23:14:01 -0400

    import code for searching dirs

--- clean_useless_files/main.py
+++ clean_useless_files/main.py
@@ -3,135 +3,63 @@ Remove listed files that are not needed in dirs
 
 """
 import os
-#import timeit
-import datetime
+import csv
+from time import strftime, gmtime
 
 
-from time import strftime, gmtime, sleep
+class File_data:
+    def __init__(self, subdir, dirs, file_name):
+        self.subdir = subdir
+        self.dirs = dirs
+        self.file_name = file_name
+        self.file_path = self.subdir + os.sep + self.file_name
+
+    def file_extension_type(self):
+        return os.path.splitext(self.file_path)
+
+    def access_file_year(self):
+        access_date_date = os.stat(self.file_path).st_mtime
+        file_year = strftime("%Y", gmtime(access_date_date))
+        return file_year
+
+    def size_file_KB(self):
+        return os.stat(self_path).st_size
+
+    def __str__(self):
+        return self.file_name
 
 
-def split_join_string(dir_path):
-    path_sting = dir_path.split(':\\')
-    s = '-'
-    return s.join(path_sting)
+def take_inventory_dir(dir_path, file_requirements):
 
+    file_path_error = 0
 
-def save_data_csv(dict_stat, current_date):
-    dir_check = split_join_string(dict_stat['file_path'])
-
-    current_work_dir = os.getcwd()
-    saved_dir_file_name = f"{current_work_dir}\\checks_{dir_check} _{current_date}.csv"
-
-    try:
-        with open(saved_dir_file_name, 'w') as csv_file:
-
-            for [key, val] in dict_stat.items():
-                csv_file.write("%s,%s\n" % (key, val))
-
-            print(f"\nCSV_Saved: \n{saved_dir_file_name}\n")
-
-    except IOError:
-        print("I/O error")
-
-    return
-
-
-def print_time_took(time):
-    """ print the correct time unit for the execution code """
-    print(time)
-    if time > 60000000:
-        print(str(round(time/60000000, 2)) + " Minutes")
-
-    if time > 1000000:
-        print(str(round(time / 1000000, 2)) + " Seconds")
-
-    else:
-        print(str(round(time, 2)) + " MicroSeconds")
-
-
-def year_file_create(file):
-    access_date_file = os.stat(file).st_mtime
-    file_access_year = strftime("%Y", gmtime(access_date_file))
-    return file_access_year
-
-
-def file_size_KB(file):
-    file_KB_size = os.stat(file).st_size
-    return file_KB_size
-
-
-def terminal_print(specs):
-    print("Rundown of Search")
-    for key, val in specs.items():
-        sleep(0.3)
-        print(f"{key} -> {val}")
-
-
-def remove_files(file):
-    try:
-        print(f"REMOVED -> /N{file} ")
-        os.remove(file)
-    except PermissionError:
-        print("Error while deleting file ", file)
-
-    return
-
-
-def search_dir(dir_path, inputs_bond):
-
-    search_info = {}
-    access_date = {}
-    extension_count = {}
-
-    accumulated_file_size = 0
-    files_path_error = 0
+    other_files = []
+    questionable_files = []
+    junk_files = []
 
     for subdir, dirs, files in os.walk(dir_path):
-
         for file_name in files:
-
             try:
-                file_path = subdir + os.sep + file_name
+                file_spec = File_data(subdir, dirs, file_name)
+                file_type_extension = file_spec.file_extension_type()
 
-                # TODO-> check files that are 2 or so years old,
-                # TODO-> check duplicate files
-                for extensions in inputs_bond["extension_no_need"]:
-                    """ 
-                    Reviews the file extension, if found in list will
-                     documentspecs and proceed to remove from the directory
-                    """
-                    if file_path.endswith(extensions):
-
-                        # extension count
-                        extension_count[extensions] = extension_count.get(
-                            extensions, 0) + 1
-
-                        # find the date the file was last accessed
-                        file_access_last = year_file_create(file_path)
-                        access_date[file_access_last] = access_date.get(
-                            file_access_last, 0) + 1
-
-                        # count the file size
-                        file_size = file_size_KB(file_path)
-                        accumulated_file_size += file_size
-
-                        # REMOVE FILES ONCE DATA IS STORED
-                        if inputs_bond['want_to_remove']:
-                            print('will remove!!!')
-                            remove_files(file_path)
-
+                if file_type_extension in file_requirements["review"]:
+                    questionable_files.append(file_spec)
+                elif file_type_extension in file_requirements["junk"]:
+                    questionable_files.append(file_spec)
+                else:
+                    other_files.append(file_spec)
             except FileNotFoundError:
-                files_path_error += 1
+                file_path_error += 1
 
-    search_info.update(file_path=dir_path)
-    search_info.update(GB_size=round(
-        accumulated_file_size/1000000000, 2))  # convert to GB
-    search_info.update(failed_path=files_path_error)
-
-    search_info.update(extension_count)
-    search_info.update(access_date)
-
-    return search_info
+    # not sure if most efficient but it works ??
+    file_bundle = {
+        "file_error": file_path_error,
+        "questionable": questionable_files,
+        "junk": junk_files,
+        "other": other_files
+    }
+    return file_bundle
 
 
 def main():
@@ -140,23 +68,20 @@ def main():
     date_check = now_date_time.strftime("%Y-%m-%d")
 
     boundaries = {
-        "want_to_remove": False,
-        "extension_no_need": [".html", ".out", ".res", ".stat",
-                              ".mesg", ".mvw"],
-        "extension_remove_old": [".op2", ".odb", ".hyp", ".h3d"]
+        "junk_file": [".html", ".out", ".res", ".stat",
+                      ".mesg", ".mvw"],
+        "review_file": [".op2", ".odb", ".hyp", ".h3d"]
     }
 
     raw_dir_path = r"F:"
-#    raw_dir_path = r"S:/v2"
+    dir_evaluation = take_inventory_dir(raw_dir_path, boundaries)
 
-    found_files_info = search_dir(raw_dir_path, boundaries)
-    terminal_print(found_files_info)
-    save_data_csv(found_files_info, date_check)
 
-    return found_files_info
+"""
+lets open the possibility to add some libs
+Bokeh, or some other data vis
+"""
 
 
 if __name__ == "__main__":
     main()
-#    script_time = timeit.timeit(main, number=1,globals=globals())
-#    print_time_took(script_time)
---
+++ clean_useless_files/v2_code_base/main_v2.py
@@ -0,0 +1,162 @@
+"""
+Remove listed files that are not needed in dirs
+
+"""
+import os
+#import timeit
+import datetime
+
+
+from time import strftime, gmtime, sleep
+
+
+def split_join_string(dir_path):
+    path_sting = dir_path.split(':\\')
+    s = '-'
+    return s.join(path_sting)
+
+
+def save_data_csv(dict_stat, current_date):
+    dir_check = split_join_string(dict_stat['file_path'])
+
+    current_work_dir = os.getcwd()
+    saved_dir_file_name = f"{current_work_dir}\\checks_{dir_check} _{current_date}.csv"
+
+    try:
+        with open(saved_dir_file_name, 'w') as csv_file:
+
+            for [key, val] in dict_stat.items():
+                csv_file.write("%s,%s\n" % (key, val))
+
+            print(f"\nCSV_Saved: \n{saved_dir_file_name}\n")
+
+    except IOError:
+        print("I/O error")
+
+    return
+
+
+def print_time_took(time):
+    """ print the correct time unit for the execution code """
+    print(time)
+    if time > 60000000:
+        print(str(round(time/60000000, 2)) + " Minutes")
+
+    if time > 1000000:
+        print(str(round(time / 1000000, 2)) + " Seconds")
+
+    else:
+        print(str(round(time, 2)) + " MicroSeconds")
+
+
+def year_file_create(file):
+    access_date_file = os.stat(file).st_mtime
+    file_access_year = strftime("%Y", gmtime(access_date_file))
+    return file_access_year
+
+
+def file_size_KB(file):
+    file_KB_size = os.stat(file).st_size
+    return file_KB_size
+
+
+def terminal_print(specs):
+    print("Rundown of Search")
+    for key, val in specs.items():
+        sleep(0.3)
+        print(f"{key} -> {val}")
+
+
+def remove_files(file):
+    try:
+        print(f"REMOVED -> /N{file} ")
+        os.remove(file)
+    except PermissionError:
+        print("Error while deleting file ", file)
+
+    return
+
+
+def search_dir(dir_path, inputs_bond):
+
+    search_info = {}
+    access_date = {}
+    extension_count = {}
+
+    accumulated_file_size = 0
+    files_path_error = 0
+
+    for subdir, dirs, files in os.walk(dir_path):
+
+        for file_name in files:
+
+            try:
+                file_path = subdir + os.sep + file_name
+
+                # TODO-> check files that are 2 or so years old,
+                # TODO-> check duplicate files
+                for extensions in inputs_bond["extension_no_need"]:
+                    """ 
+                    Reviews the file extension, if found in list will
+                     documentspecs and proceed to remove from the directory
+                    """
+                    if file_path.endswith(extensions):
+
+                        # extension count
+                        extension_count[extensions] = extension_count.get(
+                            extensions, 0) + 1
+
+                        # find the date the file was last accessed
+                        file_access_last = year_file_create(file_path)
+                        access_date[file_access_last] = access_date.get(
+                            file_access_last, 0) + 1
+
+                        # count the file size
+                        file_size = file_size_KB(file_path)
+                        accumulated_file_size += file_size
+
+                        # REMOVE FILES ONCE DATA IS STORED
+                        if inputs_bond['want_to_remove']:
+                            print('will remove!!!')
+                            remove_files(file_path)
+
+            except FileNotFoundError:
+                files_path_error += 1
+
+    search_info.update(file_path=dir_path)
+    search_info.update(GB_size=round(
+        accumulated_file_size/1000000000, 2))  # convert to GB
+    search_info.update(failed_path=files_path_error)
+
+    search_info.update(extension_count)
+    search_info.update(access_date)
+
+    return search_info
+
+
+def main():
+
+    now_date_time = datetime.datetime.now()
+    date_check = now_date_time.strftime("%Y-%m-%d")
+
+    boundaries = {
+        "want_to_remove": False,
+        "extension_no_need": [".html", ".out", ".res", ".stat",
+                              ".mesg", ".mvw"],
+        "extension_remove_old": [".op2", ".odb", ".hyp", ".h3d"]
+    }
+
+    raw_dir_path = r"F:"
+#    raw_dir_path = r"S:/v2"
+
+    found_files_info = search_dir(raw_dir_path, boundaries)
+    terminal_print(found_files_info)
+    save_data_csv(found_files_info, date_check)
+
+    return found_files_info
+
+
+if __name__ == "__main__":
+    main()
+#    script_time = timeit.timeit(main, number=1,globals=globals())
+#    print_time_took(script_time)
